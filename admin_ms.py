cec0c7eb5b5b016b98243b5f9e9fad1befe0dce2
Charles Ment <cm1099@exeter.ac.uk>
2025-03-07 14:29:48 +0000

    updated functionality and error handling of microservices

--- admin_ms.py
+++ admin_ms.py
@@ -1,8 +1,8 @@
 import sqlite3
 from flask import Flask, request
+import os
 
-
-AUDD_KEY = ''
+AUDD_KEY = os.environ["AUDD_KEY"]
 URI = "https://api.audd.io/"
 
 app = Flask(__name__)
@@ -10,88 +10,107 @@ app = Flask(__name__)
 # Endpoints
 # /admin/add
 # /admin/remove
-# /admin/get
-
+# /admin/list
 
 
 @app.route("/admin/add",methods=["POST"])
 def admin_add():
+    # This function adds a song the songs table in the database
 
-    if not request.get_json():
-        return {"error": "One or more fields are empty"}, 400
-
-    name = request.get_json()["name"]
-    file = request.get_json()["audio"]
-    artist = request.get_json()["artist"]
+    name = request.get_json().get("name")
+    file = request.get_json().get("audio") 
+    artist = request.get_json().get("artist") 
 
     con = sqlite3.connect("songs.db")
     cur = con.cursor()
 
+    # Every song must have at least a name, artist and file otherwise it would be an invalid song.
     if not (file and name and artist):
-        cur.close()
+        con.close()
         return {"error": "One or more fields are empty"}, 400
 
-    # Potentially could do an SQL injection
     query = "INSERT INTO songs VALUES (?, ?, ?)"
     try:
         cur.execute(query, (name, artist, file))
     except sqlite3.IntegrityError as e:
+        # An integrity error will be raised if the song already exists in the songs table
         con.close()
         return {"error" : "Song already exists in table"}, 409
     
-
     con.commit()
     con.close()
 
-    # Returns the row that was just created in the database
     return "", 201
 
-@app.route("/admin/remove",methods=["POST"])
+@app.route("/admin/remove",methods=["DELETE"])
 def admin_remove():
     
-    name = request.get_json()["name"]
-    artist = request.get_json()["artist"]
+    name = request.get_json().get("name")
+    artist = request.get_json().get("artist")
+
+    if not (name and artist):
+        return {"error": "One or more fields are missing"}, 400
 
     con = sqlite3.connect("songs.db")
     con.row_factory = sqlite3.Row
 
     cur = con.cursor()
-    fields = [col[0] for col in cur.description]
+    
 
     # Check if the song exists in the database before deleting it
-    query1 = "SELECT * FROM songs WHERE name ='"+name+"' AND artist = '"+artist+"'"
-    res = cur.execute(query1)
+    query1 = "SELECT * FROM songs WHERE name = ? AND artist = ?"
+    res = cur.execute(query1, (name, artist))
 
     # When there is no row to delete, then it will return a not found error
     row = res.fetchone()
     if not row:
         return {"error": "Song not found in table"}, 404
     
+    fields = [col[0] for col in cur.description]
+
     # Delete the row containing the song from the table
-    query = "DELETE FROM songs WHERE name = '"+name+"' AND artist = '"+artist+"'"
-    cur.execute(query)
-    
-    cur.close()
+    query = "DELETE FROM songs WHERE name = ? AND artist = ?"
+    cur.execute(query, (name, artist))
+
     con.commit()
+    con.close()
 
     # Sends back song that was just deleted in case the admin wants to access the file
     return {key: value for key, value in zip(fields, row)}, 204
 
-@app.route("/admin/get",methods=["GET"])
-def admin_list_all():
+@app.route("/admin/list",methods=["GET"])
+def admin_list_songs():
+    # Lists n number of songs from the songs table based on the num_of_songs parameter
+
+    # Default limit is all the songs
+    limit = ""
+    try:
+        if request.args.get("num_of_songs"):
+            # Will raise an error if the limit is not an integer
+            num_of_songs = int(request.args.get("num_of_songs"))
+
+            # Limit has to be non-negative as you can't list a negative number of songs
+            if num_of_songs < 0:
+                raise ValueError
+            
+            limit = f"LIMIT {num_of_songs}"
+    except:
+        return {"error": "Invalid limit value"}, 400
+
 
     con = sqlite3.connect("songs.db")
     con.row_factory = sqlite3.Row
-
     cur = con.cursor()
 
-    query = "SELECT name, artist, file FROM songs"
+    # Adds the limit to the sql query if it exists
+    query = f"SELECT * FROM songs {limit}"
     cur.execute(query)
-    result = cur.fetchall()
 
+    result = cur.fetchall()
     fields = [col[0] for col in cur.description]
-    cur.close()
-    
+
+    con.close()
+    # Will return all fields of the database, so if other fields are added, they will also be returned
     return {"songs": [{key: value for key, value in zip(fields, song)} for song in result]}, 200
 
 
--- user_ms.py
+++ user_ms.py
@@ -4,7 +4,7 @@ import requests
 from flask import Flask, request
 import sqlite3
 
-AUDD_KEY = ''
+AUDD_KEY = os.environ["AUDD_KEY"]
 URI = "https://api.audd.io/"
 
 app = Flask(__name__)
@@ -15,11 +15,13 @@ app = Flask(__name__)
 
 @app.route("/user/convert",methods=["POST"])
 def user_convert():
+    
 
-    if not request.get_json() or not request.get_json()["audio"]:
+    audio = request.get_json().get("audio")
+
+    if not audio:
         return {"error": "No audio file provided, or bad syntax"}, 400
-
-    audio = request.get_json()["audio"]
+    
     data = {
         "Content-Type": "multipart/form-data",
         "api_token" : AUDD_KEY,
@@ -27,25 +29,30 @@ def user_convert():
     }
     
     response = requests.post(URI, data=data)
-    
 
+    # Returns the error message if AUDD.io API returns an error
     if "error" in response.json().keys():
         return response.json()["error"], response.status_code
     
+    # Returns the error message if AUDD.io API does not recognise the fragment
     if not response.json()["result"]: 
         return {"error": "Fragment not recognised"}, 404
     
     name = response.json()["result"]["title"]
+    artist = response.json()["result"]["artist"]
 
     con = sqlite3.connect("songs.db")
     con.row_factory = sqlite3.Row
-
     cur = con.cursor()
 
-    query = "SELECT * FROM songs WHERE name ='"+name+"'"
-    res = cur.execute(query)
+    # Selects names that may not be exact matches as AUDD.io may not return the exact name
+    query = "SELECT * FROM songs WHERE name LIKE ? AND artist = ?"
+    res = cur.execute(query, (name+"%", artist))
+
     song = res.fetchone()
     fields = [col[0] for col in cur.description]
+
+    con.close()
 
     if not song:
         return {"error":"Fragment recognised but not in the songs table"}, 404
